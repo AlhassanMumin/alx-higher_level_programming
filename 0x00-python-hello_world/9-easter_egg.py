45056fc5fa645cb222c43fc216e7dc779328479c
Mumin8 <alhassanmumin8@gmail.com>
2022-09-19 15:36:07 -0700

    print The Zen of Python, by Tim Peters

--- 0x00-python-hello_world/9-easter_egg.py
+++ 0x00-python-hello_world/9-easter_egg.py
@@ -20,4 +20,4 @@ song = '''The Zen of Python, by Tim Peters
  If the implementation is hard to explain, it\'s a bad idea.
  If the implementation is easy to explain, it may be a good idea.
  Namespaces are one honking great idea -- let's do more of those!'''
-    print(f"{song}")
+print(f"{song}")
