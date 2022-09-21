92f52961199ab346a0dac6177baa69a40a70262b
Mumin8 <alhassanmumin8@gmail.com>
2022-09-20 17:20:13 -0700

    power

--- 0x01-python-if_else_loops_functions/11-pow.py
+++ 0x01-python-if_else_loops_functions/11-pow.py
@@ -2,9 +2,9 @@
 def pow(a, b):
     res = 1
     if b < 0:
-        for i in range(-b - 1):
-            a *= a
-        return (1 / a)
+        for i in range(-b):
+            res *= a
+        return (1 / res)
     elif b == 0:
         return (1)
     else:
