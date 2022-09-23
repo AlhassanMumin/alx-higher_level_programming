8b22b656c1f35a1e70140783a6b2d08713d97f8d
Mumin8 <alhassanmumin8@gmail.com>
2022-09-23 15:32:27 -0700

    add commandline argument

--- 0x02-python-import_modules/3-infinite_add.py
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -1,8 +1,10 @@
 #!/bin/python3
+# 3-infinite_add.py
+
 if __name__ == "__main__":
     """add the argument"""
     import sys
-    
+
     result = 0
     for ind in range(len(sys.argv)):
         if ind >= 1:
