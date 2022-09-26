443b716258b1407fa8ce351a9e35af2edc2bc414
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 10:53:44 -0700

    add values of tupples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# 7-add_tuple.py
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    """add two tuples"""
+
+    if len(tuple_a) < 2:
+        if len(tuple_a) == 0:
+            tuple_a = (0, 0)
+        else:
+            tuple_a = (tuple_a[0], 0)
+    if len(tuple_b) < 2:
+        if len(tuple_b) == 0:
+            tuple_b = (0, 0)
+        else:
+            tuple_b = (tuple_b[0], 0)
+    
+    tuple_sum_1stVal = tuple_a[0] + tuple_b[0]
+    tuple_sum_2ndVal = tuple_a[1] + tuple_b[1]
+    tuple_resultant = tuple_sum_1stVal, tuple_sum_2ndVal
+    
+    return tuple_resultant
