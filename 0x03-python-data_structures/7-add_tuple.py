599f6ed19004d7ae60caf006e3e0ae836de1b02b
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 10:58:07 -0700

    add values of tupples

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -14,9 +14,9 @@ def add_tuple(tuple_a=(), tuple_b=()):
             tuple_b = (0, 0)
         else:
             tuple_b = (tuple_b[0], 0)
-    
+
     tuple_sum_1stVal = tuple_a[0] + tuple_b[0]
     tuple_sum_2ndVal = tuple_a[1] + tuple_b[1]
     tuple_resultant = tuple_sum_1stVal, tuple_sum_2ndVal
-    
+
     return tuple_resultant
