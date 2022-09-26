cebbe865238d7e938ed90267a3b759e763accf32
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 13:13:39 -0700

    delete a value at  an index

--- 0x03-python-data_structures/11-delete_at.py
+++ 0x03-python-data_structures/11-delete_at.py
@@ -5,7 +5,6 @@ def delete_at(my_list=[], idx=0):
     """delete at an index"""
 
     length = len(my_list)
-    if idx < 0 or idx >= length:
-        return my_liist
-    del my_list[idx]
+    if idx >= 0 and idx < len(my_list):
+        del my_list[idx]
     return my_list
