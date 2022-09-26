9d5d1f09e3844585c2b2713df7a00ad7dd62d7ec
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 11:53:49 -0700

    delete a numver at an index

---
+++ 0x03-python-data_structures/11-delete_at.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# 11-delete_at.py
+def delete_at(my_list=[], idx=0):
+    """delete at an index"""
+
+    length = len(my_list)
+    if idx < 0 or idx >= length:
+        return my_liist
+    del my_list[idx]
+    return my_list
