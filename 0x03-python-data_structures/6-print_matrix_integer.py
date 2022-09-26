334afaed8a714c9e8d837c27ad9307eccf2c7883
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 06:13:25 -0700

    print a matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 # 6-print_matrix_integer.py
+
 def print_matrix_integer(matrix=[[]]):
     """Prints a matrix"""
+
     if matrix != [[]]:
         for row in matrix:
             row_len = len(row)
