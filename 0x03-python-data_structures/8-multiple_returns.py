18faaedf128bf281a3d88724f136e022bfac8cf3
Mumin8 <alhassanmumin8@gmail.com>
2022-09-26 11:30:19 -0700

    return a tuple

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -3,10 +3,9 @@
 
 def multiple_returns(sentence):
     """reurns a tuple """
-
-        length = len(sentence)
-        if length == 0:
-            tup = (0, None)
-        else:
-            tup = (length, sentence[0])
+    length = len(sentence)
+    if length == 0:
+        tup = (0, None)
+    else:
+        tup = (length, sentence[0])
     return tup
