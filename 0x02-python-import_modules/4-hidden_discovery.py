6d28ad6f6e2612442610d4ecc9f5ed50b189917e
Mumin8 <alhassanmumin8@gmail.com>
2022-09-27 14:21:44 -0700

    using the dir

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -3,7 +3,6 @@ if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
     import hidden_4
 
-    lists = (hidden_4)
-    for name in dir(lists):
+    for name in dir(hidden_4):
         if name[:2] != "__":
             print(name)
