92ab320878092b4bfa628dad5aeae7e9a42a4fa5
Mumin8 <alhassanmumin8@gmail.com>
2022-09-28 16:31:58 -0700

    multiply a list

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,9 +1,3 @@
 #!/usr/bin/python3
-# 11-multiply_list_map.py
-
 def multiply_list_map(my_list=[], number=0):
-    """multiply entire list"""
-
-    mult_by_4 = list(map(lambda val: val*4, my_list))
-
-    return mult_by_4
+    return list(map(lambda val: val*4, my_list))
