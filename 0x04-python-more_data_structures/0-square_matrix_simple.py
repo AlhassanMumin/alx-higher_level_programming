d9fcf57725410e00d83067812ef83194c9f2dbc0
Mumin8 <alhassanmumin8@gmail.com>
2022-09-28 15:21:17 -0700

    squares a matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -5,6 +5,7 @@ def square_matrix_simple(matrix=[]):
     """squares elements of a matrix"""
 
     new_matrix = []
+    
     for row in matrix:
         new_matrix += [list(map(lambda x : x**2, row))]
     return new_matrix
