8728fafd576ab156d41032e6bb5bf78036d7643a
Mumin8 <alhassanmumin8@gmail.com>
2022-09-30 11:19:46 -0700

    best score

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -6,11 +6,10 @@ def best_score(a_dictionary):
 
     if a_dictionary == None:
         return None
-    
+
     biggest_val = list(a_dictionary.keys())[0]
-    
+
     for key, val in a_dictionary.items():
         if val > a_dictionary.get(biggest_val):
             biggest_val = key
-            return biggest_val
-    return None 
+    return biggest_val 
