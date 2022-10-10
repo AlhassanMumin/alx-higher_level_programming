c10bcfe47edc4ec55954540156597859e230c7de
Mumin8 <alhassanmumin8@gmail.com>
2022-10-10 14:27:30 -0700

    print integer values

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+# 1-safe_print_integer.py
+
+def safe_print_integer(value):
+    """print integer values
+
+    args:
+        value: the value passed in
+
+   Return: true or  false
+   """
+    TorF = False
+    try:
+        print("{:d}".format(value))
+        TorF = True
+    except ValueError:
+        print
+    return TorF
