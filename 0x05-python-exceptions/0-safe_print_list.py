f90a6731efeef95d9b48d9a0a2f0f6ad34eca2ce
Mumin8 <alhassanmumin8@gmail.com>
2022-10-10 14:10:32 -0700

    print list

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,15 +1,22 @@
 #!/usr/bin/python3
 # 0-safe_print_list.py
-if __name__ == "__main__":
-    """print a list"""
-    
-    def safe_print_list(my_list=[], x=0):
-        num_elem = 0
-        for i in range(x):
-            try:
-                print("{}".format(my_list[i]), end="")
-                num_elem += 1
-            except IndexError:
-                print
-        print()
-        return num_elem
+
+def safe_print_list(my_list=[], x=0):
+    """Print x elememts of a list.
+
+    Args:
+        my_list (list): The list to print elements from.
+        x (int): The number of elements of my_list to print.
+
+    Returns:
+        The number of elements printed.
+    """
+    num_elem = 0
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            num_elem += 1
+        except IndexError:
+            print
+    print()
+    return num_elem
