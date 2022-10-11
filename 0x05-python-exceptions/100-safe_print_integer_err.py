5204c197b67e062a4942c1a8394dd369ccc2b55a
Mumin8 <alhassanmumin8@gmail.com>
2022-10-10 17:27:21 -0700

    print integer and stderr

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+import sys
+
+def safe_print_integer_err(value):
+    """print integers or stder
+
+    args:
+        value: the value to print
+
+    Return: Either a true or a false
+    """
+    TorF = False
+    try:
+        print("{:d}".format(value))
+        TorF = True
+    except (ValueError, TypeError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
+    return TorF
