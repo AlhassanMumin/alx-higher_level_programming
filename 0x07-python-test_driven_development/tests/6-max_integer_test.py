aff0747827bcc37c4450825a92a33339aa210118
Mumin8 <alhassanmumin8@gmail.com>
2022-10-18 03:02:37 -0700

    test folder

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,20 +7,26 @@ max_integer = __import__('6-max_integer').max_integer
 
 class TestMaxInteger(unittest.TestCase):
     def test_defaultOne(self):
+        """test for ideal case"""
         self.assertAlmostEqual(max_integer([1, 2, 3, 4]), 4) 
 
     def test_defaultTwo(self):
+        """test for ideal case"""
         self.assertEqual(max_integer([1, 3, 4, 2]), 4)
 
     def test_emptyList(self):
+        """test for an empty list"""
         self.assertEqual(max_integer([]), None)
 
     def test_withNegative(self):
+        """Test for both positive and inegative"""
         self.assertAlmostEqual(max_integer([1, 3, -4, 29, -37]), 29)
 
     def test_withFloats(self):
+        """test for only floating values"""
         self.assertEqual(max_integer([1.3379, 0.03, -4.433, 2.32]), 2.32)
 
     def test_withInfinite(self):
+        """test with an infinite value"""
         self.assertAlmostEqual(max_integer([1, 3, 4, 2, float('inf')]), float('inf'))
 
