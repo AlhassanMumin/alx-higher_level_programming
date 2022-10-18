3b808de30ac34b9156d0779972fcfdef94cb9f20
Mumin8 <alhassanmumin8@gmail.com>
2022-10-17 20:54:37 -0700

    :"

--- 0x08-python-more_classes/7-rectangle.py
+++ 0x08-python-more_classes/7-rectangle.py
@@ -1,16 +1,21 @@
 #!/usr/bin/python3
+# 6-rectangle.py
+"""representing the class"""
 
 class Rectangle:
+    """representing the rectangle"""
     number_of_instances = 0
     print_symbol = "#"
 
     def __init__(self, width=0, height=0):
+        """initialize the rectangle"""
         self.width = width
         self.height = height
         Rectangle.number_of_instances += 1
 
     @property
     def width(self):
+        """get the width of the retaingle"""
         return self.__width
 
     @width.setter
@@ -23,6 +28,7 @@ class Rectangle:
 
     @property
     def height(self):
+        """get the height of the rectangle"""
         return self.__height
 
     @height.setter
@@ -34,25 +40,31 @@ class Rectangle:
         self.__height = value
 
     def area(self):
+        """calculate the area of a rectangle"""
         return self.__width * self.__height
 
     def perimeter(self):
+        """calculate the perimeter of a rectangle"""
         if self.__width == 0 or self.__height == 0:
             return 0
         else:
             return 2*(self.__width + self.__height)
 
     def __str__(self):
+        """returns a printable representation of the rectangle"""
         self.__stri = ""
         for char in range(self.__height):
             for symb in range(self.__width):
                 self.__stri += Rectangle.print_symbol
             self.__stri += "\n"
         return self.__stri
-
+     
+     
     def __repr__(self):
+        """returns a string representation of rectangle"""
         return "Rectangle(" + str(self.__width) + ", " + str(self.__height) + ")"
 
     def __del__(self):
+        """delete an instanc e of a rectangle"""
         print("Bye rectangle...")
         Rectangle.number_of_instances -= 1
