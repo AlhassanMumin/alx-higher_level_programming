5bbf8e23589874756093f990b6bc87ce625103cb
Mumin8 <alhassanmumin8@gmail.com>
2022-10-25 18:08:26 -0700

    read text file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+# 0-read_file.py
+"""Defines a read file function"""
+
+def read_file(filename=""):
+    """prints a file content"""
+
+    with open(filename, encoding="utf-8") as f:
+        print(f.read(), end="")        
