2a263381df0a7d889a18138d2d765b4792be87e4
Mumin8 <alhassanmumin8@gmail.com>
2022-10-27 02:19:32 -0700

    read a .txt file

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -5,5 +5,5 @@
 def read_file(filename=""):
     """prints a file content"""
 
-    with open(filename, encoding="utf-8") as f:
+    with open(filename, 'r', encoding="utf-8") as f:
         print(f.read(), end="")        
