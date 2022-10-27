be32f92db70551fdb5be12ff542657260ff6556d
Mumin8 <alhassanmumin8@gmail.com>
2022-10-27 08:47:42 -0700

    dict represtation

---
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# 8-class_to_json.py
+""" dictionary description with simple data structure"""
+
+
+def class_to_json(obj):
+     """Return the dictionary represntation of a simple data structure."""
+    return obj.__dict__
