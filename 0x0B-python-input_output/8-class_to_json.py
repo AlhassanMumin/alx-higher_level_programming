b1330d98cb595ef42eeb9a535c010699724d1820
Mumin8 <alhassanmumin8@gmail.com>
2022-10-27 09:10:31 -0700

    dictionary description of class

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 # 8-class_to_json.py
-""" dictionary description with simple data structure"""
+"""Defines a Python class-to-JSON function."""
 
 def class_to_json(obj):
      """Return the dictionary represntation of a simple data structure."""
