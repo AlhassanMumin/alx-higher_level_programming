c0fb95ab05d76f761fadbf774d6f34c27d1446c0
Mumin8 <alhassanmumin8@gmail.com>
2022-10-28 14:09:32 -0700

    calculator

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,28 +1,27 @@
 #!/usr/bin/python3
-#100-my_calculator.py
-if __name__=="__main__":
+# 100-my_calculator.py
+if __name__ == "__main__":
     """It handles basic operations"""
     import sys
     from calculator_1 import add, sub, mul, div
 
-    
     if len(sys.argv) - 1 != 3:
         print("Usage: ./100-my_calculator.py <a> <operator> <b>")
         sys.exit(1)
-    
+
     first = int(sys.argv[1])
     second = int(sys.argv[3])
-    
+
     add = add(first, second)
     sub = sub(first, second)
     mul = mul(first, second)
     div = div(first, second)
-    
-    if sys.argv[2] == "+":
+
+    if ord(sys.argv[2]) == ord('+'):
         print("{} + {} = {}".format(sys.argv[1], sys.argv[3], add))
     elif sys.argv[2] == "-":
         print("{} - {} = {}".format(sys.argv[1], sys.argv[3], sub))
-    elif sys.argv[2] == "*":
+    elif ord(sys.argv[2]) == ord('*'):
         print("{} * {} = {}".format(sys.argv[1], sys.argv[3], mul))
     elif sys.argv[2] == "/":
         print("{} / {} = {}".format(sys.argv[1], sys.argv[3], div))
