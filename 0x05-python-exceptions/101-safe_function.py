ebe42337af47a945f6c4301b7b0b3de1883e9bd0
Mumin8 <alhassanmumin8@gmail.com>
2022-10-29 02:13:46 -0700

    safe functions

---
+++ 0x05-python-exceptions/101-safe_function.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+# 101-safe_function.py
+"""Defines a print safely function"""
+import sys
+
+
+def safe_function(fct, *args):
+    """print safely function definition
+
+    Args:
+        fct (function): the passed in function
+        args (argument): variable argument passed
+
+    Return:
+            the returned value on success
+    """
+    try:
+        return fct(args[0], args[1])
+    except (ZeroDivisionError, IndexError):
+        print("Exception: {}".format(sys.exc_info()[1]), file=sys.stderr)
