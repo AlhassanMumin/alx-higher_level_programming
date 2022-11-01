a86724f2462675f71e8fbb2cc26c19eae56d7662
Mumin8 <alhassanmumin8@gmail.com>
2022-11-01 13:06:23 -0700

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -85,7 +85,7 @@ class Rectangle(Base):
 
     def area(self):
         """computes the area of the triangle"""
-        return self.__width*self.__height
+        return self.__width * self.__height
 
     def display(self):
         """Display a rectangle with a '#'"""
