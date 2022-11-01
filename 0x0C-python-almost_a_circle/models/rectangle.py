23ebb60511cd5e476087c219a8453a7b8d88729b
Mumin8 <alhassanmumin8@gmail.com>
2022-10-31 17:54:17 -0700

    models

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 # rectangle.py
 """the definition of the rectangle class"""
-
 from models.base import Base
 
 
@@ -9,6 +8,7 @@ class Rectangle(Base):
     """the rectangle lass"""
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """the constructor"""
         self.width = width
         self.height = height
         self.x = x
