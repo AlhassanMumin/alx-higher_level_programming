afb314b23d1a058223e7b7e3702220b2c7e581d2
Mumin8 <alhassanmumin8@gmail.com>
2022-11-01 14:31:57 -0700

    models

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -3,6 +3,21 @@
 from models.rectangle import Rectangle
 
 class Square(Rectangle):
+    """Represent a square."""
+
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(size, size, x, y, id)
+        """Initialize a new Square.
+        Args:
+            size (int): The size of the new Square.
+            x (int): The x coordinate of the new Square.
+            y (int): The y coordinate of the new Square.
+            id (int): The identity of the new Square.
+        """        
+        Rectangle.__init__(self, size, size, x, y, id)
+
+    def __str__(self):
+        """the magic str method"""
+        return "[Square] ({}) {}/{} - {}".format(
+                self.id, self.x, self.y, self.width)
+
 
