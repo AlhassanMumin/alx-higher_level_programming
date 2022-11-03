63698365a3f2cf483eadd0fcf747ffb884b74168
Mumin8 <alhassanmumin8@gmail.com>
2022-11-03 10:05:37 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,6 +31,8 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
+         if list_dictionaries is None or list_dictionaries == []:
+             return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
