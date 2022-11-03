c227e2ac54a8453fd8683efe4030e145b62fee97
Mumin8 <alhassanmumin8@gmail.com>
2022-11-03 10:11:33 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,8 +31,8 @@ class Base:
         Args:
             list_dictionaries (list): A list of dictionaries.
         """
-         if list_dictionaries is None or list_dictionaries == []:
-             return "[]"
+        if list_dictionaries is None or list_dictionaries == []:
+            return "[]"
         return json.dumps(list_dictionaries)
 
     @classmethod
@@ -46,6 +46,5 @@ class Base:
             if list_objs is None:
                 jf.write("[]")
             else:
-                [print("{}".format(o.to_dictionary())) for o in list_objs]
                 dict_list = [o.to_dictionary() for o in list_objs]
                 jf.write(Base.to_json_string(dict_list))
