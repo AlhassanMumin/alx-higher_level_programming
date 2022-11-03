332eae2b038145d81985b1cead3a41ec053ae0ec
Mumin8 <alhassanmumin8@gmail.com>
2022-11-03 10:03:18 -0700

    models

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 # base.py
 """Definition of the class base"""
+import json
+from io import StringIO
 
 
 class Base:
@@ -22,3 +24,26 @@ class Base:
         else:
             Base.__nb_objects += 1
             self.id = Base.__nb_objects
+
+    @staticmethod
+    def to_json_string(list_dictionaries):
+        """converts a list of dictionary to json string
+        Args:
+            list_dictionaries (list): A list of dictionaries.
+        """
+        return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """save_to_file a json representation
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, 'w') as jf:
+            if list_objs is None:
+                jf.write("[]")
+            else:
+                [print("{}".format(o.to_dictionary())) for o in list_objs]
+                dict_list = [o.to_dictionary() for o in list_objs]
+                jf.write(Base.to_json_string(dict_list))
