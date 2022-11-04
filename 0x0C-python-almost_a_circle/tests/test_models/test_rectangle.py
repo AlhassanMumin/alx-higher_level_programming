e4601bc916b2268e28db4028982cfc99d69d5336
Mumin8 <alhassanmumin8@gmail.com>
2022-11-04 12:23:28 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -5,7 +5,8 @@ import unittest
 from models.base import Base
 from models.rectangle import Rectangle
 
-class TestRectangleInstance(unittest.TestCase):
+
+class TestRectangle_Instance(unittest.TestCase):
     """Unittests for testing instantiation of the Rectangle class."""
 
     def test_rectangle_is_Base(self):
@@ -14,3 +15,40 @@ class TestRectangleInstance(unittest.TestCase):
     def test_two_args(self):
         r1 = Rectangle(12, 2)
         self.assertEqual(r1.id, 2)
+
+    def test_without_args(self):
+        with self.assertRaises(TypeError):
+            r1 = Rectangle()
+
+    def test_two_args(self):
+        r1 = Rectangle(4, 45)
+        r = Rectangle(4, 45)
+        r2 = Rectangle(4, 45)
+        self.assertEqual(r1.id, r2.id - 2)
+
+    def test_with_idvalue(self):
+        r3 = Rectangle(10, 2, 0, 0, 12)
+        self.assertEqual(r3.id, 12)
+
+    def test_height_string(self):
+        with self.assertRaisesRegex(TypeError, 'height must be an integer'):
+            h_string = Rectangle(4, "height")
+
+    def test_set_negative_width(self):
+        with self.assertRaisesRegex(ValueError, 'width must be > 0'):
+            r = Rectangle(10, 2)
+            r.width = -10
+
+    def test_pass_negative_y(self):
+        with self.assertRaisesRegex(ValueError, 'y must be > 0'):
+            Rectangle(10, 2, 3, -1)
+
+    def test_set_x_as_dictionary(self):
+        with self.assertRaisesRegex(TypeError, "x must be an integer"):
+            r = Rectangle(10, 2)
+            r.x = {}
+
+    def test_set_y_as_dictionary(self):
+        with self.assertRaisesRegex(TypeError, "y must be an integer"):
+            r = Rectangle(10, 2)
+            r.y = {}
