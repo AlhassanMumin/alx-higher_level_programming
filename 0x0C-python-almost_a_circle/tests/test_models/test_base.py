9eb60c6975a2d07a546dd430e4fba130802cfdab
Mumin8 <alhassanmumin8@gmail.com>
2022-11-04 10:06:27 -0700

    tests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 """Define test base case"""
 import unittest
-
 from models.base import Base
 
 
@@ -9,24 +8,19 @@ class TestBaseClassForInstances(unittest.TestCase):
     """The test case class"""
 
     def test_Noneinstance(self):
-        # testing normal instance of the Base
         b1 = Base()
         self.assertEqual(b1.id, 1)
 
     def test_no_argument(self):
-        # Adding two more instances
         b1 = Base()
         b2 = Base()
         self.assertEqual(b2.id, 3)
 
     def test_with_value_instance(self):
-        # Passing a positive integer as a value
         b3 = Base(12)
         self.assertEqual(12, b3.id)
 
     def test_passNone(self):
-        # Passing a None as argument
-
         b4 = Base(None)
         self.assertEqual(b4.id, 4)
 
@@ -59,7 +53,7 @@ class TestBaseClassForInstances(unittest.TestCase):
         self.assertEqual(True, Base(True).id)
 
     def test_dictionary(self):
-        self.assertEqual({'1':1, '2':2}, Base({'1':1, '2':2}).id)
+        self.assertEqual({'1': 1, '2': 2}, Base({'1': 1, '2': 2}).id)
 
     def test_list(self):
         self.assertEqual([1, 2, 3], Base([1, 2, 3]).id)
@@ -69,7 +63,7 @@ class TestBaseClassForInstances(unittest.TestCase):
 
     def test_bytes(self):
         self.assertEqual(bytes(3), Base(b'\x00\x00\x00').id)
-    
+
     def test_byteArray(self):
         self.assertEqual(bytearray(3), Base((b'\x00\x00\x00')).id)
 
@@ -85,5 +79,3 @@ class TestBaseClassForInstances(unittest.TestCase):
 
     def test_NaN(self):
         self.assertNotEqual(Base(float('nan')).id, float('nan'))
-
-
