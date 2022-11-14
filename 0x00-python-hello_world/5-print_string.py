d990fb96ed0904aba0f655426a505118796ae364
Mumin8 <alhassanmumin8@gmail.com>
2022-11-14 01:46:36 -0800

    print strings

--- 0x00-python-hello_world/5-print_string.py
+++ 0x00-python-hello_world/5-print_string.py
@@ -1,3 +1,5 @@
 #!/usr/bin/python3
 str = "Holberton School"
-print('{}{}{}\n{}'.format(str, str, str, str[:9]))
+# The code in comment produce the same result
+# print('{}{}{}\n{}'.format(str, str, str, str[0:-7]))
+print('{}\n{}'.format(str * 3, str[:9]))
