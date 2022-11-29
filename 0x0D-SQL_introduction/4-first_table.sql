9b9e6e62d2139a1f1b74b1d8af27f70e3ddf897f
Mumin8 <alhassanmumin8@gmail.com>
2022-11-29 15:45:15 -0800

    create database

--- 0x0D-SQL_introduction/4-first_table.sql
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -1,3 +1,5 @@
 -- creates a table called first_table in the database btcp_0c_0 in MySQL server
-CREATE TABLE IF NOT EXISTS first_table
-	   (id INT, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS first_table(
+id INT, 
+name VARCHAR(256)
+);
