26e8e783f9bba3adb41e374d8b673a8e695982bc
Mumin8 <alhassanmumin8@gmail.com>
2022-11-30 14:49:17 -0800

    create user with all priveileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,7 @@
 -- create user user_0d_1
 CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED WITH caching_sha2_password BY 'user_0d_1_pwd';
+IDENTIFIED WITH caching_sha2_password 
+BY 'user_0d_1_pwd';
+GRANT SELECT 
+ON *.* 
+TO 'user_0d_1'@localhost;
