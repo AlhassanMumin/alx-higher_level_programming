000530fce45646648dc9b5056eb644544bb666e3
Mumin8 <alhassanmumin8@gmail.com>
2022-11-30 15:24:32 -0800

    create user and grant privileges

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,7 +1,9 @@
 -- create user user_0d_1
-CREATE USER IF NOT EXISTS 'user_0d_1'@'localhost' 
-IDENTIFIED WITH caching_sha2_password 
-BY 'user_0d_1_pwd';
-GRANT SELECT 
-ON *.* 
-TO 'user_0d_1'@localhost;
+CREATE USER 
+IF NOT EXISTS 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd';
+GRANT SELECT
+ON *.*
+TO 'user_0d_1'@'localhost'
+IDENTIFIED BY 'user_0d_1_pwd'
+FLUSH PRIVILEGES;
