f303361ff8dccf873c7c7f9c3bb77d7a28a7a0de
Mumin8 <alhassanmumin8@gmail.com>
2023-01-17 14:34:16 -0800

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -10,4 +10,3 @@ if __name__ == "__main__":
         print('    - type: {}'.format(type(the_page)))
         print('    - content: {}'.format(the_page))
         print('    - utf8 content: {}'.format(the_page.decode("utf-8")))
-
