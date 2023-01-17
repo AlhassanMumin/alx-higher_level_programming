d86367e03541a4629a70284e510fb5b3bfd05e5e
Mumin8 <alhassanmumin8@gmail.com>
2023-01-17 15:00:29 -0800

    fetch url

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# this script fetches https://alx-intranet.hbtn.io/status
+"""this script fetches https://alx-intranet.hbtn.io/status"""
 import urllib.request
 
 if __name__ == "__main__":
