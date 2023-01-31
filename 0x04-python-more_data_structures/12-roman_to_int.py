4c1cd4212ce64fc42aeb9c6def4b5a19dbb13992
Mumin8 <alhassanmumin8@gmail.com>
2023-01-31 14:50:03 -0800

    roman to int

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,22 @@
+def roman_to_int(roman_string):
+    values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+            }
+    result = 0
+
+    if not isinstance(roman_string, str) or roman_string is None:
+        return 0
+
+    for i in range(len(roman_string)):
+        cur_val = values[roman_string[i]]
+        if i + 1 < len(roman_string) and values[roman_string[i + 1]] > cur_val:
+            result -= cur_val
+        else:
+            result += cur_val
+    return result
