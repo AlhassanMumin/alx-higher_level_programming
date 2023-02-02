be036fd326e2fefa87f0c84bdff736f54ad74450
Mumin8 <alhassanmumin8@gmail.com>
2023-02-02 12:23:28 -0800

    weighted average

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -5,7 +5,7 @@ def weight_average(my_list=[]):
     """finds the average of tuples"""
 
     prod, Sum, result = 0, 0, 0
-    if my_list is None:
+    if my_list is None or len(my_list) is 0:
         return 0
     for tup in my_list:
         prod += tup[0]*tup[1]
