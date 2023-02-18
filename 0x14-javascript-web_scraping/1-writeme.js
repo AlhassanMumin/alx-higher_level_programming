53fb3f995db32bbe0a24195baaddb49714260615
AlhassanMumin <alhassanmumin8@gmail.com>
2023-02-18 14:43:51 -0800

    WRITEME"

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -4,7 +4,7 @@ const fs = require('fs');
 const args = process.argv.slice(2);
 fs.writeFile(args[0], args[1], { encoding: 'utf8' }, (err) => {
   if (err) {
-    console.error(err);
+    console.log(err);
     return;
   }
 });
