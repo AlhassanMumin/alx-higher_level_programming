71ac6813829b1f1bd507a3f6929ea9381ac80872
AlhassanMumin <alhassanmumin8@gmail.com>
2023-02-19 02:55:04 -0800

    starwars count

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = process.argv[2]
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error:', error);
+  } else {
+    const films = JSON.parse(body)['results'];
+    let numMoviesWithWedge = 0;
+
+    films.forEach((film) => {
+      const characters = film.characters;
+	    if (characters.includes('https://swapi-api.alx-tools.com/api/people/18/')) {
+        numMoviesWithWedge++;
+      }
+    });
+
+    console.log(`${numMoviesWithWedge}`);
+  }
+});
