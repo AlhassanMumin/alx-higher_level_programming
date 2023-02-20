2c650cb1f8d7f0907155f1fae1fea09b86f67632
AlhassanMumin <alhassanmumin8@gmail.com>
2023-02-19 16:08:14 -0800

    completed tasks

---
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+const request = require('request');
+const apiURL = process.argv[2];
+request(apiURL, (error, response, body) => {
+  if (error) {
+    console.error(error);
+    return;
+  }
+const todos = JSON.parse(body);
+const completedTasksByUserId = new Map();
+for (const todo of todos) {
+    if (todo.completed) {
+      const userId = todo.userId;
+      const count = completedTasksByUserId.get(userId) || 0;
+      completedTasksByUserId.set(userId, count + 1);
+    }
+  }
+
+const myDictionary = new Object();
+  for (const [userId, count] of completedTasksByUserId.entries()) {
+    myDictionary[userId] = count;	  
+  }
+	console.log(myDictionary);
+});
